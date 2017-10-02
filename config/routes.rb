729ff0419fddfd4b6da55b5cd2379576e5452ae6
niklaus-git <nbausw@gmail.com>
2017-10-02 13:23:50 +0200

    routes fix

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-root 'creatives/index'
-get 'creatives/index'
-get 'creatives/show'
-
+root to: 'creatives#index'
+get 'creatives/index', to: 'creatives#index'
+get 'creatives/show', to: 'creatives#show'
 end
+
